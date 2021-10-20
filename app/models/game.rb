1b4bf3bf2290e2ef081540f832fc789b5465f767
keitakitamoto <keita0410.k.k@gmail.com>
2021-10-20 10:10:06 +0000

    layout2

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,6 +8,23 @@ class Game < ApplicationRecord
     likes.where(user_id: user.id).exists?
   end
   #ログインしているユーザーがこのゲームをいいねしているかどうかをチェックする記述
+  
+  def avg_score
+    unless self.reviews.empty?
+      reviews.average(:rate).round(1).to_f
+    else
+      0.0
+    end
+  end
+  
+  def review_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:rate).round(1).to_f*100/5
+    else
+      0.0
+    end
+  end
+
 
   attachment :image
 
