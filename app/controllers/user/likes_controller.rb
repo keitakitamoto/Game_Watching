e7a2700da39ca85794c798491fadf4cbcf3e94aa
EC2 Default User <ec2-user@ip-172-31-40-167.ap-northeast-1.compute.internal>
2021-11-01 05:51:21 +0000

    非同期通信

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -17,15 +17,15 @@ class User::LikesController < ApplicationController
   end
 
   def create
-    game = Game.find(params[:game_id])
-    like = current_user.likes.new(game_id: game.id)
+    @game = Game.find(params[:game_id])
+    like = current_user.likes.new(game_id: @game.id)
     like.save
     #redirect_to user_game_path(game)
   end
 
   def destroy
-    game = Game.find(params[:game_id])
-    like = current_user.likes.find_by(game_id: game.id)
+    @game = Game.find(params[:game_id])
+    like = current_user.likes.find_by(game_id: @game.id)
     like.destroy
     #redirect_to user_game_path(game)
   end
